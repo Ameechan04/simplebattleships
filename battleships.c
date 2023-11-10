ae7bb7aabe60c27584a01461383aea457a7f8221
Andrew Meechan <95620148+Ameechan04@users.noreply.github.com>
2023-11-10 21:27:14 +0000

    Update battleships.c

--- battleships.c
+++ battleships.c
@@ -10,9 +10,12 @@
 #include <stdlib.h>
 //#include "functions.h"
 int main (void) {
-    char x, y;
+    int rows = 10;
+    int columns = 12;
+    
+    int x, y;
  //Create the board
-  char board[10][12] = 
+ char board[10][12] = 
         {{'9', ' ', '\0', '\0','\0','\0','\0','\0','\0','\0', '\0'},	//0
         {'8', ' ','\0','\0','\0','\0','\0','\0','\0', '\0', '\0'},	//1
 	    {'7', ' ', '\0','\0','\0','\0','\0','\0','\0', '\0', '\0'},	//2
@@ -24,7 +27,13 @@ int main (void) {
 	 {'1', ' ', '\0', '\0','\0','\0','\0','\0','\0','\0', '\0'},	//8
 		{' ', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}};	//9
 
+ 
+
+  
+
  printf("\n SIMPLE CHESS BY ANDREW MEECHAN \n \n");
+
+
 
  //print the board
 		for (int r = 0; r < 10; r++) {
@@ -35,8 +44,8 @@ int main (void) {
 		}
 		
 printf("Enter a position to mark as your ship. \n");
-scanf("%d", x);
-scanf("%d", y);
+scanf(" %d", &x);
+scanf(" %d", &y);
 board[x][y] = 'X';
 
 	for (int r = 0; r < 10; r++) {
@@ -45,6 +54,8 @@ board[x][y] = 'X';
 			}
 			printf("\n");
 		}
+		
+
 
 return 0;
 }
