03c2880e2238d8da0dfdfc565b8fd698a81c4fc9
Andrew Meechan <95620148+Ameechan04@users.noreply.github.com>
2023-11-10 20:33:58 +0000

    Create battleships.c

---
+++ battleships.c
@@ -0,0 +1,18 @@
+/* 
+ * A simple game of battleships
+ * Author: Andrew Meeehan <andrew.meechan.2022@uni.strath.ac.uk>
+ * Program is to create a 2D Array to represent a battleship board for two players
+ * Each turn the player will choose a target to shoot at and will be told if it hits or not
+ * Last updated: 07/10/23
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+
+
+
+
+return 0;
+}
